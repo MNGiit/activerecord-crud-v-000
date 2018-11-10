858b870947b3831df4139fc32c8feeae82b882c5
Mohammed Noori <kmanoorib@gmail.com>
2018-11-10 02:44:11 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,6 +61,7 @@ def can_find_by_multiple_attributes
   # release_date == 2000
   # director == "Me"
   Movie.find_by(title:"Title", release_date:2000, director:"Me")
+  #.find_by can take more than 1 arg, so that's cool
 end
 
 def can_find_using_where_clause_and_be_sorted
